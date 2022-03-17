a34f5e8bdeb402369e73017418212f99ced31e2b
Nicolas Hamelin <n.hamelin44120@gmail.com>
2022-03-17 16:13:44 +0100

    remove rebus async

--- CakeMachine/Simulation/RemoveRebus.cs
+++ CakeMachine/Simulation/RemoveRebus.cs
@@ -1,4 +1,5 @@
-﻿using CakeMachine.Fabrication;
+﻿using System.Runtime.CompilerServices;
+using CakeMachine.Fabrication;
 using CakeMachine.Fabrication.Elements;
 
 namespace CakeMachine.Simulation
@@ -7,6 +8,9 @@ namespace CakeMachine.Simulation
     {
         /// <inheritdoc />
         public override bool SupportsSync => true;
+        
+        /// <inheritdoc />
+        public override bool SupportsAsync => true;
 
         /// <inheritdoc />
         public override IEnumerable<GâteauEmballé> Produire(Usine usine, CancellationToken token)
@@ -52,29 +56,57 @@ namespace CakeMachine.Simulation
                 }
             }
         }
+
+        /// <inheritdoc />
+        public override async IAsyncEnumerable<GâteauEmballé> ProduireAsync(Usine usine,
+            [EnumeratorCancellation] CancellationToken token)
+        {
+            var postePréparation = usine.Préparateurs.Single();
+            var posteCuisson = usine.Fours.Single();
+            var posteEmballage = usine.Emballeuses.Single();
+            
+            while (!token.IsCancellationRequested)
+            {
+                var plat = new Plat();
+                
+                if (plat.EstConforme)
+                {
+                    var gâteauCru = usine.Préparateurs.First().Préparer(plat);
+
+                    if (gâteauCru.EstConforme)
+                    {
+                        var gâteauCuit = usine.Fours.First().Cuire(gâteauCru).Single();
+
+                        if (gâteauCuit.EstConforme)
+                        {
+                            var gâteauEmballé = usine.Emballeuses.First().Emballer(gâteauCuit);
+
+                            if (gâteauEmballé.EstConforme)
+                            {
+                                yield return gâteauEmballé;
+                            }
+                            else
+                            {
+                                plat = new Plat();
+                            }
+                        }
+                        else
+                        {
+                            plat = new Plat();
+                        }
+                    }
+                    else
+                    {
+                        plat = new Plat();
+                    }
+                }
+                else
+                {
+                    plat = new Plat();
+                }
+            }
+        }
     }
 }
 
-/*
- var plat = new Plat();
-                do
-                {
-                    plat = new Plat();
-                } while (!plat.EstConforme);
-                var gâteauCru = usine.Préparateurs.First().Préparer(plat);
-                do
-                {
-                    gâteauCru = usine.Préparateurs.First().Préparer(plat);
-                } while (!gâteauCru.EstConforme);
-                var gâteauCuit = usine.Fours.First().Cuire(gâteauCru).Single();
-                do
-                {
-                    gâteauCuit = usine.Fours.First().Cuire(gâteauCru).Single();
-                } while (!gâteauCuit.EstConforme);
-                var gâteauEmballé = usine.Emballeuses.First().Emballer(gâteauCuit);
-                do
-                {
-                    gâteauEmballé = usine.Emballeuses.First().Emballer(gâteauCuit);
-                } while (!gâteauEmballé.EstConforme);
-                yield return gâteauEmballé;
- */
+
