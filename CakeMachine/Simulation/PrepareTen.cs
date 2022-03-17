d0e0a185c64360c41137ec0f2e9f6479ea6bf660
jnamdegone <83634445+jnamdegone-yperia@users.noreply.github.com>
2022-03-17 11:21:19 +0100

    RemoveRebus & PrepareTen

---
+++ CakeMachine/Simulation/PrepareTen.cs
@@ -0,0 +1,57 @@
+﻿using CakeMachine.Fabrication;
+using CakeMachine.Fabrication.Elements;
+
+namespace CakeMachine.Simulation
+{
+    internal class PrepareTen : Algorithme
+    {
+        /// <inheritdoc />
+        public override bool SupportsSync => true;
+
+        /// <inheritdoc />
+        public override IEnumerable<GâteauEmballé> Produire(Usine usine, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var plat = new Plat();
+                List<GâteauCru> ListeGâteauCrus = new List<GâteauCru>();
+                List<GâteauCuit> ListeGâteauCuits = new List<GâteauCuit>();
+                List<GâteauEmballé> ListeGâteauEmballés = new List<GâteauEmballé>();
+
+                do
+                {
+                    var gâteauCru = usine.Préparateurs.First().Préparer(plat);
+                    ListeGâteauCrus.Add(gâteauCru);
+                } while (ListeGâteauCrus.Count == 10 );
+                do
+                {
+                    foreach (GâteauCru g in ListeGâteauCrus)
+                    {
+
+                        var gâteauCuit = usine.Fours.First().Cuire(g).Single();
+                        ListeGâteauCuits.Add(gâteauCuit);
+                    }
+                } while (ListeGâteauCuits.Count == 10 );
+                do
+                {
+                    foreach (GâteauCuit gc in ListeGâteauCuits)
+                    {
+                        var gâteauEmballé = usine.Emballeuses.First().Emballer(gc);
+                        ListeGâteauEmballés.Add(gâteauEmballé);
+                    }
+                } while (ListeGâteauCrus.Count == 10 );
+
+
+             
+                foreach(GâteauEmballé ge in ListeGâteauEmballés)
+                {
+                    yield return ge;
+                }
+                   
+                
+
+            }
+        }
+    }
+}
+
---
+++ CakeMachine/Simulation/RemoveRebus.cs
@@ -0,0 +1,40 @@
+﻿using CakeMachine.Fabrication;
+using CakeMachine.Fabrication.Elements;
+
+namespace CakeMachine.Simulation
+{
+    internal class RemoveRebus : Algorithme
+    {
+        /// <inheritdoc />
+        public override bool SupportsSync => true;
+
+        /// <inheritdoc />
+        public override IEnumerable<GâteauEmballé> Produire(Usine usine, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var plat = new Plat();
+                do
+                {
+                    plat = new Plat();
+                } while (!plat.EstConforme);
+                var gâteauCru = usine.Préparateurs.First().Préparer(plat);
+                do
+                {
+                    gâteauCru = usine.Préparateurs.First().Préparer(plat);
+                } while (!gâteauCru.EstConforme);
+                var gâteauCuit = usine.Fours.First().Cuire(gâteauCru).Single();
+                do
+                {
+                    gâteauCuit = usine.Fours.First().Cuire(gâteauCru).Single();
+                } while (!gâteauCuit.EstConforme);
+                var gâteauEmballé = usine.Emballeuses.First().Emballer(gâteauCuit);
+                do
+                {
+                    gâteauEmballé = usine.Emballeuses.First().Emballer(gâteauCuit);
+                } while (!gâteauEmballé.EstConforme);
+                yield return gâteauEmballé;
+            }
+        }
+    }
+}
