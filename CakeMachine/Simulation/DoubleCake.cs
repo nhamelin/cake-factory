425a0b83322b6829d78c1999f5bf5198939fa1d1
jnamdegone <83634445+jnamdegone-yperia@users.noreply.github.com>
2022-03-17 11:28:48 +0100

    Refacto DoubleCake

--- CakeMachine/Simulation/DoubleCake.cs
+++ CakeMachine/Simulation/DoubleCake.cs
@@ -1,31 +1,34 @@
 using CakeMachine.Fabrication;
 using CakeMachine.Fabrication.Elements;
 
-namespace CakeMachine.Simulation;
-
-internal class DoubleCake : Algorithme
+namespace CakeMachine.Simulation
 {
-    public override bool SupportsSync => true;
 
-    /// <inheritdoc />
-    public override IEnumerable<GâteauEmballé> Produire(Usine usine, CancellationToken token)
+
+    internal class DoubleCake : Algorithme
     {
-        while (!token.IsCancellationRequested)
+        public override bool SupportsSync => true;
+
+        /// <inheritdoc />
+        public override IEnumerable<GâteauEmballé> Produire(Usine usine, CancellationToken token)
         {
-            var plat1 = new Plat();
-            var plat2 = new Plat();
+            while (!token.IsCancellationRequested)
+            {
+                var plat1 = new Plat();
+                var plat2 = new Plat();
 
-            var gâteauCru1 = usine.Préparateurs.First().Préparer(plat1);
-            var gâteauCru2 = usine.Préparateurs.First().Préparer(plat2);
-            
-            var gâteauCuit1 = usine.Fours.First().Cuire(gâteauCru1).Single();
-            var gâteauCuit2 = usine.Fours.First().Cuire(gâteauCru2).Single();
-            
-            var gâteauEmballé1 = usine.Emballeuses.First().Emballer(gâteauCuit1);
-            var gâteauEmballé2 = usine.Emballeuses.First().Emballer(gâteauCuit2);
+                var gâteauCru1 = usine.Préparateurs.First().Préparer(plat1);
+                var gâteauCru2 = usine.Préparateurs.First().Préparer(plat2);
 
-            yield return gâteauEmballé1;
-            yield return gâteauEmballé2;
+                var gâteauCuit1 = usine.Fours.First().Cuire(gâteauCru1).Single();
+                var gâteauCuit2 = usine.Fours.First().Cuire(gâteauCru2).Single();
+
+                var gâteauEmballé1 = usine.Emballeuses.First().Emballer(gâteauCuit1);
+                var gâteauEmballé2 = usine.Emballeuses.First().Emballer(gâteauCuit2);
+
+                yield return gâteauEmballé1;
+                yield return gâteauEmballé2;
+            }
         }
     }
 }
