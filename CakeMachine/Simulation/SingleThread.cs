7a1627455ea243ad7d881256df15c8bebc797ca7
nairod49 <78696310+nairod49@users.noreply.github.com>
2022-03-18 16:48:15 +0100

    dernier comit

--- CakeMachine/Simulation/SingleThread.cs
+++ CakeMachine/Simulation/SingleThread.cs
@@ -1,6 +1,8 @@
 ﻿using System.Runtime.CompilerServices;
 using CakeMachine.Fabrication;
 using CakeMachine.Fabrication.Elements;
+using CakeMachine.Fabrication.Opérations;
+using CakeMachine.Utils;
 
 namespace CakeMachine.Simulation
 {
@@ -10,20 +12,35 @@ namespace CakeMachine.Simulation
         public override bool SupportsSync => true;
 
         /// <inheritdoc />
-        public override bool SupportsAsync => true;
+        public override bool SupportsAsync => false;
 
         /// <inheritdoc />
         public override IEnumerable<GâteauEmballé> Produire(Usine usine, CancellationToken token)
         {
-            var postePréparation = usine.Préparateurs.Single();
-            var posteCuisson = usine.Fours.Single();
-            var posteEmballage = usine.Emballeuses.Single();
+            var builder = new UsineBuilder();
+            builder.NombreEmballeuses = 10;
+            builder.NombreFours = 2;
+            builder.NombrePréparateurs = 10;
+            var usine2 = builder.Build();
+            
+            var postePréparation = usine2.Préparateurs.Single();
+            var posteCuisson = usine2.Fours.Single();
+            var posteEmballage = usine2.Emballeuses.Single();
 
             while (!token.IsCancellationRequested)
             {
-                var plat = new Plat();
+                var plats = Enumerable.Range(0, 10).Select(_ => new Plat());
+                foreach (var plat in plats)
+                {
+                    var gâteauCru = plats.AsParallel().Select(postePréparation.Préparer);
 
-                var gâteauCru = postePréparation.Préparer(plat);
+                    foreach (var gat in gâteauCru)
+                    {
+                        var gâteauxCuits = CuireParLots(gat, posteCuisson, capacitéFour);
+                    }
+                }
+
+                
                 var gâteauCuit = posteCuisson.Cuire(gâteauCru).Single();
                 var gâteauEmballé = posteEmballage.Emballer(gâteauCuit);
                 
@@ -50,4 +67,17 @@ namespace CakeMachine.Simulation
             }
         }
     }
+
+    private static IEnumerable<GâteauCuit> CuireParLots(IEnumerable<GâteauCru> gâteaux, Cuisson four, uint capacitéFour)
+    {
+        var queue = new Queue<GâteauCru>(gâteaux);
+
+        while (queue.Any())
+        {
+            var gâteauxCuits = four.Cuire(queue.Dequeue(capacitéFour).ToArray());
+            foreach (var gâteauCuit in gâteauxCuits)
+                yield return gâteauCuit;
+        }
+    }
+
 }
