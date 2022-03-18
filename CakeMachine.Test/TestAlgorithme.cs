698fb4021f9976d82394417bf140c028bb709cd6
Enzo Sandré <atelier@enzosandre.fr>
2022-03-18 13:10:25 +0100

    Algorithme optimisé

--- CakeMachine.Test/TestAlgorithme.cs
+++ CakeMachine.Test/TestAlgorithme.cs
@@ -1,6 +1,6 @@
 using System;
-using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using CakeMachine.Fabrication;
 using CakeMachine.Simulation;
 using Xunit;
@@ -10,23 +10,15 @@ namespace CakeMachine.Test
     public class TestAlgorithme
     {
         [Fact]
-        public void TestSingleThread()
+        public async Task TestAlgoOptimisé()
         {
-            var singleThread = new SingleThread();
+            var singleThread = new Optimisée1Poste();
             var usine = new UsineBuilder().Build();
 
             var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10));
-            singleThread.Produire(usine, cancellationTokenSource.Token).ToArray();
-        }
-
-        [Fact]
-        public void TestDixParDix()
-        {
-            var algo = new DixParDix();
-            var usine = new UsineBuilder().Build();
-
-            var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10));
-            algo.Produire(usine, cancellationTokenSource.Token).ToArray();
+            await foreach(var _ in singleThread.ProduireAsync(usine, cancellationTokenSource.Token))
+            {
+            }
         }
     }
 }
